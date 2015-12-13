374178d2b259b89e09ec9b58e68c88f45d2c6edb
Dmitry Avershin <dmitraver@gmail.com>
2015-12-13 21:34:53 +0100

    Day 3 part 2.

--- src/main/scala/com/github/dmitraver/adventofcode/DayThree.scala
+++ src/main/scala/com/github/dmitraver/adventofcode/DayThree.scala
@@ -17,11 +17,21 @@ object DayThree {
   }
 
   def main(args: Array[String]) {
-    val stream = ResourceLoader.fromResource("day3").toStream
-    println("Houses: " + countHouses(stream))
+    val streamOne = ResourceLoader.fromResource("day3").toStream
+    println("Houses: " + countHouses(streamOne))
+
+    val streamTwo = ResourceLoader.fromResource("day3").toStream
+    println("Houses with robo:" + countHousesWithRobo(streamTwo))
   }
 
   def countHouses(stream: Stream[Char]) = {
     stream.map(toCoordinate).scan(Coordinate(0, 0))(_ + _).toSet.size
+  }
+
+  def countHousesWithRobo(stream: Stream[Char]) = {
+    val (santa, roboSanta) = stream.zipWithIndex.partition(_._2 % 2 == 0)
+    val santaSet = santa.map(indexedCoordinate => toCoordinate(indexedCoordinate._1)).scan(Coordinate(0, 0))(_ + _).toSet
+    val roboSantaSet = roboSanta.map(indexedCoordinate => toCoordinate(indexedCoordinate._1)).scan(Coordinate(0, 0))(_ + _).toSet
+    santaSet.union(roboSantaSet).size
   }
 }
--- src/test/scala/DayThreeTest.scala
+++ src/test/scala/DayThreeTest.scala
@@ -10,4 +10,10 @@ class DayThreeTest extends FunSuite with Matchers {
     DayThree.countHouses(Source.fromString("^>v<").toStream) shouldBe 4
     DayThree.countHouses(Source.fromString("^v^v^v^v^v").toStream) shouldBe 2
   }
+
+  test("Count houses with robo should return correct result") {
+    DayThree.countHousesWithRobo(Source.fromString("^v").toStream) shouldBe 3
+    DayThree.countHousesWithRobo(Source.fromString("^>v<").toStream) shouldBe 3
+    DayThree.countHousesWithRobo(Source.fromString("^v^v^v^v^v").toStream) shouldBe 11
+  }
 }
