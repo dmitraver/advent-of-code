ce7cec819f613fa8f26c05ca70f4340ebdb28842
dmitraver <dmitraver@gmail.com>
2015-12-23 17:39:56 +0100

    Day 15. Parsing input into objects.

---
+++ src/main/scala/com/github/dmitraver/adventofcode/DayFifteen.scala
@@ -0,0 +1,19 @@
+package com.github.dmitraver.adventofcode
+
+import com.github.dmitraver.adventofcode.utils.ResourceLoader
+
+case class Ingredient(name: String, capacity: Int, durability: Int, flavor: Int, texture: Int, calories: Int)
+
+object DayFifteen {
+
+  val pattern = "([a-zA-Z]+): capacity (-?[0-9]+), durability (-?[0-9]+), flavor (-?[0-9]+), texture (-?[0-9]+), calories (-?[0-9]+)".r
+
+  def main(args: Array[String]) {
+    val ingredients = ResourceLoader.fromResource("day15").getLines().map{
+      case pattern(name, capacity, durability, flavor, texture, calories) =>
+        Ingredient(name, capacity.toInt, durability.toInt, flavor.toInt, texture.toInt, calories.toInt)
+    }
+  }
+
+
+}
