d8851c16fcad6814e1e1c19830b3eb7c5dd7d2c1
Dmitry Avershin <dmitraver@gmail.com>
2016-12-06 15:08:46 +0100

    Day 4 part 2.

--- src/main/scala/com/github/dmitraver/adventofcode/y2016/DayFour.scala
+++ src/main/scala/com/github/dmitraver/adventofcode/y2016/DayFour.scala
@@ -7,14 +7,39 @@ import scala.io.Source
 object DayFour {
 
   val idAndChecksumPattern = "([0-9]+)\\[([a-zA-Z]+)\\]".r
+  val letters = ('a' to 'z').toArray
 
   def main(args: Array[String]) {
-    val source = ResourceLoader.fromResource("y2016/day4")
-    println("Sum of sector ids of real rooms: " + sumOfRealRoomsSectorIds(source))
+    val sourceOne = ResourceLoader.fromResource("y2016/day4")
+    println("Sum of sector ids of real rooms: " + sumOfRealRoomsSectorIds(sourceOne))
+
+    val sourceTwo = ResourceLoader.fromResource("y2016/day4")
+    println("Sector id of north pole objects room: " + findSectorIdOfNorthPoleObjectsRoom(sourceTwo))
   }
 
   def sumOfRealRoomsSectorIds(source: Source) = {
     source.getLines().map(toRoom).filter(isReal).foldLeft(0)((sum, room) => sum + room.id)
+  }
+
+  def findSectorIdOfNorthPoleObjectsRoom(source: Source) = {
+    source.getLines().map(toEncryptedRoomNameWithId).map(roomNameWithId => (decryptRoomName(roomNameWithId._1, roomNameWithId._2), roomNameWithId._2)).find(roomWithId => isNorthPoleObjectsRoom(roomWithId._1)).get._2
+  }
+
+  def toEncryptedRoomNameWithId(encryptedData: String) = {
+    val parts = encryptedData.split("-")
+    val idAndChecksumPattern(id, checksum) = parts.last
+    val roomNameParts = parts.dropRight(1)
+    (roomNameParts, id.toInt)
+  }
+
+  def decryptRoomName(roomName: Array[String], sectorId: Int) = roomName.map(str => rotateEveryCharOfString(str, sectorId)).mkString(" ")
+
+  def isNorthPoleObjectsRoom(roomName: String) = roomName == "northpole object storage"
+
+  def rotateEveryCharOfString(str: String, rotations: Int) = str.map(c => rotateChar(c, rotations)).mkString
+
+  def rotateChar(c: Char, rotations: Int): Char = {
+    letters((letters.indexOf(c) + rotations % letters.length) % letters.length)
   }
 
   def isReal(room: Room) = {
@@ -28,8 +53,6 @@ object DayFour {
     val idAndChecksumPattern(id, checksum) = parts.last
     Room(encryptedName.mkString(""), id.toInt, checksum)
   }
-
-
 }
 
 case class Room(encryptedName: String, id: Int, checksum: String)
