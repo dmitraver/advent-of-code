31e0cc14c8a6448bbfa1be9efc953d1af51a5f13
Dmitry Avershin <dmitraver@gmail.com>
2017-12-02 12:14:55 +0100

    Day 2 part 2.

--- src/main/scala/com/github/dmitraver/adventofcode/y2017/DayTwo.scala
+++ src/main/scala/com/github/dmitraver/adventofcode/y2017/DayTwo.scala
@@ -4,17 +4,38 @@ import com.github.dmitraver.adventofcode.utils.ResourceLoader
 
 object DayTwo {
 
-  def calculateChecksum(input: Iterator[String]): Int = {
+  val minMaxDiffCalc = (row: Vector[Int]) => row.max - row.min
+
+  def calculateChecksum(input: Stream[String])(row: Vector[Int] => Int): Int = {
     input.map { str =>
-      val nums = str.split("\\s+").map(_.toInt)
-      nums.max - nums.min
+      val nums = str.split("\\s+").map(_.toInt).toVector
+      row(nums)
     }.sum
   }
 
+  def evenlyDivisibleCalc(input: Vector[Int]): Int = {
+    val sorted = input.sorted
+
+    def loop(index: Int): Int = {
+      if(index == sorted.length) -1
+      else {
+        val value = sorted(index)
+        (0 until index) find(i => value % sorted(i) == 0) match {
+          case Some(x) => value / sorted(x)
+          case None => loop(index + 1)
+        }
+      }
+    }
+
+    loop(0)
+  }
+
   def main(args: Array[String]): Unit = {
-    val input = ResourceLoader.fromResource("y2017/day2")
-    val checksum = calculateChecksum(input.getLines())
-    println("Checksum: " + checksum)
+    val input = ResourceLoader.fromResource("y2017/day2").getLines().toStream
+    val checksumOne = calculateChecksum(input)(minMaxDiffCalc)
+    val checksumTwo = calculateChecksum(input)(evenlyDivisibleCalc)
+    println("Checksum one: " + checksumOne)
+    println("Checksum two: " + checksumTwo)
   }
 
 }
--- src/test/scala/y2017/DayTwoTest.scala
+++ src/test/scala/y2017/DayTwoTest.scala
@@ -7,12 +7,21 @@ import scala.io.Source
 
 class DayTwoTest extends FunSuite with Matchers {
 
-  private val input =
+  private val inputPartOne =
     """|5 1 9 5
        |7 5 3
        |2 4 6 8""".stripMargin
 
+  private val inputPartTwo =
+    """|5 9 2 8
+       |9 4 7 3
+       |3 8 6 5""".stripMargin
+
   test("Checksum part 1") {
-    DayTwo.calculateChecksum(Source.fromString(input).getLines()) shouldBe 18
+    DayTwo.calculateChecksum(Source.fromString(inputPartOne).getLines().toStream)(DayTwo.minMaxDiffCalc) shouldBe 18
+  }
+
+  test("Checksum part 2") {
+    DayTwo.calculateChecksum(Source.fromString(inputPartTwo).getLines().toStream)(DayTwo.evenlyDivisibleCalc) shouldBe 9
   }
 }
