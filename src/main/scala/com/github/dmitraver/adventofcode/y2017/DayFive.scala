7dbfae69cf48d44376b2ed7f50ebc709a08252f5
Dmitry Avershin <dmitraver@gmail.com>
2017-12-05 16:10:29 +0100

    Day 5 part 2.

--- src/main/scala/com/github/dmitraver/adventofcode/y2017/DayFive.scala
+++ src/main/scala/com/github/dmitraver/adventofcode/y2017/DayFive.scala
@@ -4,7 +4,7 @@ import com.github.dmitraver.adventofcode.utils.ResourceLoader
 
 object DayFive {
 
-  def countSteps(jumps: Vector[Int]): Int = {
+  def countSteps(jumps: Vector[Int])(offset: Int => Int): Int = {
     val instructions = jumps.toArray
 
     def loop(currentInstruction: Int = 0, steps: Int = 0): Int = {
@@ -12,7 +12,7 @@ object DayFive {
       val jumpOffsetIndex = currentInstruction + jumpOffset
       if (jumpOffsetIndex < 0 || jumpOffsetIndex >= instructions.length) steps + 1
       else {
-        instructions(currentInstruction) = jumpOffset + 1
+        instructions(currentInstruction) = offset(jumpOffset)
         loop(jumpOffsetIndex, steps + 1)
       }
     }
@@ -20,8 +20,12 @@ object DayFive {
     loop()
   }
 
+  def offsetIncrement(offset: Int): Int = offset + 1
+  def offsetDecrement(offset: Int): Int = if (offset >= 3) offset - 1 else offset + 1
+
   def main(args: Array[String]): Unit = {
     val input = ResourceLoader.fromResource("y2017/day5").getLines().map(_.toInt).toVector
-    println("Number of steps: " + countSteps(input))
+    println("Number of steps 1: " + countSteps(input)(offsetIncrement))
+    println("Number of steps 2: " + countSteps(input)(offsetDecrement))
   }
 }
--- src/test/scala/y2017/DayFiveTest.scala
+++ src/test/scala/y2017/DayFiveTest.scala
@@ -6,6 +6,10 @@ import org.scalatest.{FunSuite, Matchers}
 class DayFiveTest extends FunSuite with Matchers {
 
   test("Count steps 1") {
-    DayFive.countSteps(Vector(0, 3, 0, 1, -3)) shouldBe 5
+    DayFive.countSteps(Vector(0, 3, 0, 1, -3))(DayFive.offsetIncrement) shouldBe 5
+  }
+
+  test("Count steps 2") {
+    DayFive.countSteps(Vector(0, 3, 0, 1, -3))(DayFive.offsetDecrement) shouldBe 10
   }
 }
