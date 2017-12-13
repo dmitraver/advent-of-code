35ee723f9e4302afe7d17633add53dce8163d998
Dmitry Avershin <dmitraver@gmail.com>
2017-12-13 23:27:43 +0100

    Day 13 part 1.

--- src/main/scala/com/github/dmitraver/adventofcode/y2017/DayThirteen.scala
+++ src/main/scala/com/github/dmitraver/adventofcode/y2017/DayThirteen.scala
@@ -8,7 +8,7 @@ object DayThirteen {
   type Range = Int
   type Time = Int
 
-  val FirewallPattern = "(\\d+):\\s+(\\d+)".r
+  private val FirewallPattern = "(\\d+):\\s+(\\d+)".r
 
   def processInput(input: Vector[String]): Map[Depth, Range] = {
     input.foldLeft(Map.empty[Depth, Range]) { (acc, input) =>
@@ -18,23 +18,32 @@ object DayThirteen {
     }
   }
 
-  def scannerPositionAtTime(time: Time, layer: Depth, layers: Map[Depth, Range]): Int = {
+  private def scannerPositionAtTime(time: Time, layer: Depth, layers: Map[Depth, Range]): Int = {
     layers.getOrElse(layer, -1) match {
       case -1 => -1
       case range =>
-        val isEven = ((time / range) - 1) % 2 == 0
-        val offset = time % range
-        if (isEven) offset
-        else range - 1 - offset
+        val x = (range - 1) * 2
+        val offset = time % x
+        if (offset <= range - 1) offset
+        else x - offset
+
+    }
+  }
+
+  private def isCaught(time: Time, layers: Map[Depth, Range]): Boolean = {
+    scannerPositionAtTime(time, time, layers) == 0
+  }
+
+  def getSeverity(layers: Map[Depth, Range]): Int = {
+    (0 to layers.maxBy(_._1)._1).foldLeft(0) { (acc, time) =>
+      if(isCaught(time, layers)) acc + (time * layers(time))
+      else acc
     }
   }
 
   def main(args: Array[String]): Unit = {
     val input = ResourceLoader.fromResource("y2017/day13").getLines().toVector
     val layers = processInput(input)
-    println(scannerPositionAtTime(8, 4, layers))
-//    (0 to 6) foreach { i =>
-//      println(i + " - " + isCaught(i, firewall))
-//    }
+    println("Severity: " + getSeverity(layers))
   }
 }
---
+++ src/test/scala/y2017/DayThirteenTest.scala
@@ -0,0 +1,21 @@
+package y2017
+
+import com.github.dmitraver.adventofcode.y2017.DayThirteen
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.io.Source
+
+class DayThirteenTest extends FunSuite with Matchers {
+
+  val input =
+    """0: 3
+      |1: 2
+      |4: 4
+      |6: 4""".stripMargin
+
+  test("Severity") {
+    val layers = DayThirteen.processInput(Source.fromString(input).getLines().toVector)
+    DayThirteen.getSeverity(layers) shouldBe 24
+  }
+
+}
