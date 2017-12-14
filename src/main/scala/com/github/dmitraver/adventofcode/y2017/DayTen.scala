91cb0cae47338891d698a8c7addb5bb3f7af13d7
Dmitry Avershin <dmitraver@gmail.com>
2017-12-14 16:59:04 +0100

    Day 10 part 2.

--- src/main/scala/com/github/dmitraver/adventofcode/y2017/DayTen.scala
+++ src/main/scala/com/github/dmitraver/adventofcode/y2017/DayTen.scala
@@ -1,5 +1,6 @@
 package com.github.dmitraver.adventofcode.y2017
 
+case class RoundResult(currentPosition: Int, skipSize: Int, vector: Vector[Int])
 
 object DayTen {
 
@@ -16,22 +17,51 @@ object DayTen {
   }
 
   def hash(lengths: Vector[Int], inputLength: Int): Int = {
-    def loop(currentPosition: Int = 0, lengthIndex: Int = 0, skipSize: Int = 0, vector: Vector[Int] = Vector.range(0, inputLength)): Int = {
-      if (lengthIndex == lengths.size) vector(0) * vector(1)
+    val result = hashingRound(0, 0, Vector.range(0, inputLength), lengths).vector
+    result(0) * result(1)
+  }
+
+  def hashingRound(currentPosition: Int, skipSize: Int, vector: Vector[Int], lengths: Vector[Int]): RoundResult = {
+    def go(currentPosition: Int, lengthIndex: Int, skipSize: Int, vector: Vector[Int]): RoundResult = {
+      if (lengthIndex == lengths.size) RoundResult(currentPosition, skipSize, vector)
       else {
         val length = lengths(lengthIndex)
         val offset = currentPosition % vector.size
         val reversed = reverse(vector, offset, length)
-        loop(currentPosition + length + skipSize, lengthIndex + 1, skipSize + 1, reversed)
+        go(currentPosition + length + skipSize, lengthIndex + 1, skipSize + 1, reversed)
       }
     }
 
-    loop()
+    go(currentPosition, 0, skipSize, vector)
+
   }
+
+  private def strToBytes(str: String): Vector[Int] = str.map(_.toInt).toVector
+
+  def sparseHash(input: String) = {
+    val lengths = strToBytes(input) ++ Vector(17, 31, 73, 47, 23)
+    def go(rounds: Int, currentPosition: Int, skipSize: Int, vector: Vector[Int]): RoundResult = {
+      if (rounds == 0) RoundResult(currentPosition, skipSize, vector)
+      else {
+        val result = hashingRound(currentPosition, skipSize, vector, lengths)
+        go(rounds - 1, result.currentPosition, result.skipSize, result.vector)
+      }
+    }
+
+    go(64, 0, 0, Vector.range(0, 256))
+  }
+
+  def denseHash(input: String): String = {
+    val hash = sparseHash(input).vector
+    hash.grouped(16).map(xor).map("%02x" format _).mkString
+  }
+
+  private def xor(vector: Vector[Int]): Int = vector.foldLeft(0)((acc, i) => acc ^ i)
 
   def main(args: Array[String]): Unit = {
     val lengths = Vector(106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118)
     println("Hash:" + hash(lengths, 256))
+    println("Dense hash:" + denseHash("106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118"))
   }
 
 }
--- src/test/scala/y2017/DayTenTest.scala
+++ src/test/scala/y2017/DayTenTest.scala
@@ -8,4 +8,11 @@ class DayTenTest extends FunSuite with Matchers {
   test("Hash") {
     DayTen.hash(Vector(3, 4, 1, 5), 5) shouldBe 12
   }
+
+  test("Dense hash") {
+    DayTen.denseHash("") shouldBe "a2582a3a0e66e6e86e3812dcb672a272"
+    DayTen.denseHash("AoC 2017") shouldBe "33efeb34ea91902bb2f59c9920caa6cd"
+    DayTen.denseHash("1,2,3") shouldBe "3efbe78a8d82f29979031a4aa0b16a9d"
+    DayTen.denseHash("1,2,4") shouldBe "63960835bcdc130f0b66d7ff4f6a5a8e"
+  }
 }
