ef00a57776d5b647802fd049f549896a2778ff5f
Dmitry Avershin <dmitraver@gmail.com>
2017-12-19 18:07:52 +0100

    Day 18 experiments.

---
+++ src/main/scala/com/github/dmitraver/adventofcode/y2017/DayEighteen.scala
@@ -0,0 +1,43 @@
+package com.github.dmitraver.adventofcode.y2017
+
+//sealed trait Command
+//class SndCommand(frequency) extends Command
+//
+//sealed trait Command
+//class SndCommand extends Command {
+//  private val pattern = "snd ([\\w|\\d+]+)".r
+//
+//  def unapply(s: String): Option[(String, String, String)] = s match {
+//    case pattern(left, right, output) => Some(left, right, output)
+//    case _ => None
+//  }
+//}
+
+object DayEighteen {
+
+  type Register = Char
+
+  val sndCommandPattern = "snd ([\\w|\\d+]+)".r
+  val setCommandPattern = "set (\\w) ([\\w|\\d+]+)".r
+  val addCommandPattern = "add (\\w) ([\\w|\\d+]+)".r
+  val modCommandPattern = "mod (\\w) ([\\w|\\d+]+)".r
+  val rcvCommandPattern = "rcv ([\\w|\\d+]+)".r
+  val jgzCommandPattern = "jgz ([\\w|\\d+]+) ([\\w|\\d+]+)".r
+
+  def processCommands(commands: Vector[String]): Int = {
+    def go(commandIndex: Int, registers: Map[Char, Int]): Int = {
+      if (commandIndex < 0 || commandIndex > commands.size) {
+        -1
+      } else {
+        val command = commands(commandIndex)
+        command match {
+          case sndCommandPattern()
+        }
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
